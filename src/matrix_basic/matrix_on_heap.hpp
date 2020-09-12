04f8e6e1c882dcec10b50f4c9ee02c1145d5f843
lentychang <lentychang@gmail.com>
2020-09-13 01:12:28 +0200

    add another constructor

--- src/matrix_basic/matrix_on_heap.hpp
+++ src/matrix_basic/matrix_on_heap.hpp
@@ -1,15 +1,16 @@
 #include<iostream>
 #include<vector>
 #include<initializer_list>
+#include<tuple>
 
 template<typename T>
 class Matrix{
     std::vector<T> elems;
+    std::vector<unsigned int> shp;
     public:
-    Matrix(std::initializer_list<T> lst): elems(lst){};
+    Matrix(std::initializer_list<T> lst): elems(lst), shp{lst.size()}{};
+    // init all elements with given value
+    Matrix(unsigned int N, T val): elems{std::vector<T>(N,val)}, shp{N}{};
+    std::vector<unsigned int> shape(){ return shp;};
     const std::vector<T>& get_elems() {return elems;};
 };
-
-// int main(){
-    
-// }
--- tests/test_matrix_basic_heap.cpp
+++ tests/test_matrix_basic_heap.cpp
@@ -1,9 +1,15 @@
 #include "../src/matrix_basic/matrix_on_heap.hpp"
 #include <gtest/gtest.h>
 
-TEST(TEST_MATRIX_BASIC_HEAP,CONSTRUCTOR){
+TEST(TEST_MATRIX_BASIC_HEAP,CONSTRUCTOR_initializer_list){
     Matrix<int> m{1};
     ASSERT_EQ(m.get_elems()[0],1);
+}
+
+TEST(TEST_MATRIX_BASIC_HEAP,CONSTRUCTOR_same_value){
+    Matrix<int> m(10,12);
+    ASSERT_EQ(m.get_elems()[9],12);
+    ASSERT_EQ(m.get_elems()[8],12);
 }
 
 int main(int argc, char **argv) {
